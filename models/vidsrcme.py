4f4686263373c000333a63f30099d657a529ae68
streamamos <134351107+streamamos@users.noreply.github.com>
2024-03-16 07:36:19 +0000

    Update vidsrcme.py

--- models/vidsrcme.py
+++ models/vidsrcme.py
@@ -70,5 +70,5 @@ async def get(dbid,s=None,e=None,l='eng'):
     # ,{
     "name":'SuperEmbed',
     "sources":results[1][0] if len(results)==2 else 1500,
-    "subtitles":results[1][1] if len(results)==2 else 1500
+    "subtitles":results[1][1] if len(results)==2 else 1500,
     }]
